83825af79c6710bb49a0eeaddb9cf7b0c7682e63
Aayush Chhikara <88643777+boltx393@users.noreply.github.com>
2023-12-18 16:25:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-function search() {
-    const searchInput = document.getElementById('search-input');
-    const searchQuery = searchInput.value;
+document.addEventListener('DOMContentLoaded', function () {
+    var searchInput = document.querySelector('.search');
+    var googleSearchButton = document.querySelector('.buttons button:first-child');
+    var feelingLuckyButton = document.querySelector('.buttons button:last-child');
 
-    // You can perform an API request or any other logic with the searchQuery
+    // Function to perform the Google search
+    function performSearch() {
+        var searchTerm = searchInput.value.trim();
 
-    // For now, let's just display the search query in the search results
-    const searchResults = document.getElementById('search-results');
-    searchResults.innerHTML = `<p>Search results for: ${searchQuery}</p>`;
-}
+        if (searchTerm !== '') {
+            var googleSearchURL = 'https://www.google.com/search?q=' + encodeURIComponent(searchTerm);
+            window.location.href = googleSearchURL;
+        }
+    }
+
+    // Event listener for the Enter key on the search input
+    searchInput.addEventListener('keypress', function (event) {
+        if (event.key === 'Enter') {
+            performSearch();
+        }
+    });
+
+    // Event listener for the Google Search button
+    googleSearchButton.addEventListener('click', function () {
+        performSearch();
+    });
+
+    // Event listener for the I'm Feeling Lucky button
+    feelingLuckyButton.addEventListener('click', function () {
+        window.location.href = 'https://doodles.google/';
+    });
+});
